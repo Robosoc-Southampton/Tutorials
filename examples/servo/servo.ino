fc4837425716163a914a4522e7a20e35aea615ff
exerro <benedict.allen2514@gmail.com>
2019-10-06 18:25:17 +0100

    Add comments to servo example

--- examples/servo/servo.ino
+++ examples/servo/servo.ino
@@ -1,14 +1,20 @@
 
+// define the pin to use for the servo
 #define SERVO_PIN 5
 
+// declare the servo
 Servo servo;
 
 void setup() {
+	// attach the pin to the servo, so it knows what wire to
+	// use to control the servo
 	servo.attach(SERVO_PIN);
 }
 
 void loop() {
 	for (int i = 0; i < 10; ++i) {
+		// write a value to the servo
+		// value is in degrees (between 0 and 180 degrees)
 		servo.write(i * 10);
 		delay(50);
 	}
